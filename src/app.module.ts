10d6d81713afab42af4b7c723f9bdfa9539e132e
usman ghani <usmanghani564.ug9@gmail.com>
2022-11-02 20:50:37 +0500

    add customer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import {ProductsModule} from './product/products.module';
 import {DatabaseConfigModule} from './core/config/database/config.module';
 import {DatabaseModule} from './core/database/database.module';
+import {CustomerModule} from './customer/customer.module';
 
 @Module({
   imports: [
     ProductsModule,
+    CustomerModule,
     DatabaseModule,
     DatabaseConfigModule,
   ],
---
+++ src/customer/customer.entity.ts
@@ -0,0 +1,17 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Customer {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    address: string;
+
+}
