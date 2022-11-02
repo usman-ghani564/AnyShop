dda160f6140e42150cfb1ddfe79b271fea168d45
usman ghani <usmanghani564.ug9@gmail.com>
2022-11-02 18:26:57 +0500

    remove comments

--- src/core/config/database/configuration.ts
+++ src/core/config/database/configuration.ts
@@ -2,11 +2,6 @@ import {registerAs} from '@nestjs/config';
 
 export default registerAs('database', () => ({
     type: 'mssql',
-    // host: 'localhost',
-    // port: 6666,
-    // username: 'sa',
-    // password: 'Hussnain20',
-    // database: 'shoply',
     host: process.env.DATABASE_HOST,
     port: +process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
