8d227d67248a575d44b19ed3e016ff2cb601fcad
usman ghani <usmanghani564.ug9@gmail.com>
2022-11-04 17:18:37 +0500

    Add Jwt token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {AuthService} from './core/auth/auth.service';
 import {LocalAuthGuard} from './core/auth/local-auth.guard';
 
 @Controller()
 export class AppController {
 
+    constructor(private authService: AuthService){}
+
     @UseGuards(LocalAuthGuard)
     @Post('auth/login')
     async login(@Request() req) {
-        return req.user
+        return this.authService.login(req.user);
     }
 }
--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import {UsersService} from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) {}
+    constructor(private userService: UsersService, private jwtService: JwtService) {}
 
     async validateUser(username: string, password: string) : Promise<any> {
         const user = await this.userService.findOne(username);
@@ -13,5 +14,13 @@ export class AuthService {
             return result;
         }
         return null;
+    }
+
+    async login(user: any) {
+        const payload = {username: user.username, sub: user.userId};
+
+        return {
+            access_token: this.jwtService.sign(payload)
+        };
     }
 }
